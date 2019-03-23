ae5881b275ec39a80554fd52b00490be19c2119d
mcortz <mykortz@gmail.com>
2019-03-23 23:16:38 +0800

    initial commit

---
+++ src/main/java/com/bluflex/blurestapp/BluRestAppApplication.java
@@ -0,0 +1,13 @@
+package com.bluflex.blurestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluRestAppApplication.class, args);
+	}
+
+}
